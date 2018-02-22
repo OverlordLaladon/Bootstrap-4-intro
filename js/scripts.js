bb7bdc6bc38069087ffa5e126fc286c93c716d8c
OverlordLaladon <lalagame@fidy.hu>
2018-02-22 21:58:50 +0100

    Assignment 4

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,46 @@ $(document).ready(function(){
         $("#carousel-button").children("span").addClass('fa-pause');
         }
     });
+    $('#loginModallink').click(function(){
+        $('#loginModal').modal('show');
+    })
+    ;
+    $('#loginModalclose').click(function(){
+        $('#loginModal').modal('hide');
+    })
+    ;
+    $('#loginModalCancel').click(function(){
+        $('#loginModal').modal('hide');
+    })
+    ;
+    $('#reserveModalBtn').click(function(){
+        $('#reserveModal').modal('show');
+    })
+    ;
+    $('#reserveModalClose').click(function(){
+        $('#reserveModal').modal('hide');
+    })
+    ;
+    $('#reserveModalCancel').click(function(){
+        $('#reserveModal').modal('hide');
+    })
+    ;
 });
+
+
+/* teszt
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});
+
+$('#loginModal').click(function(){
+    $('#loginModal').modal(options)
+})
+;
+*/
